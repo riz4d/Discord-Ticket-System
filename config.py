5acf550802f5df298bdb18afad459e81f8662890
Mohamed Rizad <rizadvillan@gmail.com>
2023-09-09 15:39:57 +0530

    confs

---
+++ config.py
@@ -0,0 +1,3 @@
+server_id = 1234567891234567890 #replace Server id
+user_category_id = 1234567891234567890 # Replace the category ID where the DM tickets are stored in channels.
+discord_bot_token = "cdygcyewugcywegcygweycgweygcwtvtewcwe.fewywetcvewdtcewr" # Replace your discord bot token (https://discord.com/developers/applications)
